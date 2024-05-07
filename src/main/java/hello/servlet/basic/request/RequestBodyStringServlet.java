2ad848e0ab3f0cf73909699e4dbaa4381c25d3a2
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-05-08 00:44:40 +0900

    5월7일분

--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -1,5 +1,4 @@
 package hello.servlet.basic.request;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
@@ -7,22 +6,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.util.StreamUtils;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-
-@WebServlet(name = "requestBosyStringServlet", urlPatterns = "/request-body-string")
-public class RequestBodyStringServlet extends HttpServlet {
-
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+        public class RequestBodyStringServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ServletInputStream inputStream = request.getInputStream();//바이트코드로 가져옴
-        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);//어떤 인코딩인지 알려줘야 한다.
-
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
         System.out.println("messageBody = " + messageBody);
-
         response.getWriter().write("ok");
-
-
     }
 }
