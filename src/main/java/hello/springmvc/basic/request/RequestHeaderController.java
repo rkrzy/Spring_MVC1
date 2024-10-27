d69585e0f985f203b695922612bc63f2f18a0e30
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-10-27 12:38:26 +0900

    스프링MVC - HTTP요청/기본,헤더 조회

--- src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
+++ src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -1,2 +1,35 @@
-package hello.springmvc.basic.request;public class RequestHeaderController {
+package hello.springmvc.basic.request;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@Slf4j
+@RestController
+public class RequestHeaderController {
+
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletRequest response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestParam MultiValueMap<String, String> headerMap,
+                          @RequestHeader("host")String host,
+                          @CookieValue(value = "myCookie", required = false) String cookie)
+    {
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+
+        return "ok";
+    }
 }
