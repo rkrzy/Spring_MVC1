db2c1c33efd41dc0015201a7fb7279e6815d96a2
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-10-27 15:30:43 +0900

    HTTP요청 파라미터 - @RequestParam

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -7,8 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -22,4 +25,64 @@ public class RequestParamController {
 
         response.getWriter().write("ok");
     }
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge
+    )
+    {
+        log.info("username = {}, age = {}", memberName, memberAge);
+        return "ok";
+    }
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age
+    )
+    {
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age)
+    {
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
+    //required = false로 설정했을때 값을 안받으면 Null값을 받아야 하는데 자바는 그게 불가능하다. 따라서 int를 받겠다면 Integer로 설정해야 한다.
+    //"" 과 Null은 다른 값이다.
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age
+    )
+    {
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1 ") int age
+    )
+    {
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(
+            @RequestParam Map<String, Object> paramMap)
+    {
+        log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
 }
